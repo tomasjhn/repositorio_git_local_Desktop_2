5e40c756f5da40dfd0b1d62e286eaa950548e9e1
tomasjhn <97974371+tomasjhn@users.noreply.github.com>
2022-01-21 13:55:56 +0100

    Revert "Revert "Segunda modificación en Github Desktop""
    This reverts commit 33fe8b5aad29ef2991dbbc326022eab2d4b2720b.

--- src/Principal.java
+++ src/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
 		System.out.println("Modificacion hecha en el proyecto");
+		System.out.println("Segunda modificación hecha en el proyecto");
 	}
 
 }
