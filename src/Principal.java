1a59cd5592956a498a34ed2664b85c0209bf4edc
tomasjhn <97974371+tomasjhn@users.noreply.github.com>
2022-01-21 12:38:39 +0100

    Primera confirmación en GitHub Desktop
    Confirmación en Github Desktop

---
+++ src/Principal.java
@@ -0,0 +1,10 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+		System.out.println("Modificacion hecha en el proyecto");
+	}
+
+}
